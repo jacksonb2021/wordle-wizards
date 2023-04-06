79c2130743217e7ab4230ed677773ca70039df95
dukespeed <duke@speedkin.com>
2023-04-06 12:56:52 -0700

    Implemented guess function, returns a map of correct vs incorrect chars via integer flags.

--- src/model/Wordle.java
+++ src/model/Wordle.java
@@ -19,11 +19,35 @@ public class Wordle {
     }
 
 
-
-    public String guess(String word){
+    /**
+     * This function compares the word passed into to the dailyWord variable, set in the constructor,
+     * and returns a map of character indices for keys, and then WRONG (0), CORRECT (1) and CONTAINS (2)
+     * as the potential values.
+     * @param word The word to check
+     * @return A map with indices for keys, 0 for wrong, 1 for correct position, 2 for in the string, wrong position.
+     */
+    public HashMap<Integer, Integer> guess(String word){
         //Todo guess the word, return a string with a space for wrong characters, underscore for wrong location?
         // compare to original string??
-        return null;
+        HashMap<Integer, Integer> checkedChars = new HashMap<>();
+        final int WRONG = 0;
+        final int CORRECT = 1;
+        final int CONTAINS = 2;
+
+
+        for (int index = 0; index < word.length(); index++) {
+            char guessChar = word.charAt(index);
+            char correctChar = dailyWord.charAt(index);
+            if (guessChar == correctChar) {
+                checkedChars.put(index, CORRECT);
+            } else if (dailyWord.indexOf(guessChar) != -1) {
+                checkedChars.put(index, CONTAINS);
+            } else {
+                checkedChars.put(index, WRONG);
+            }
+
+        }
+        return checkedChars;
     }
 
 
