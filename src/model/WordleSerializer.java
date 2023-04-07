5e0d3c0206a42297320a659d1c6eac2141a1b5e8
jacksonb2021 <jacksonburns2021@gmail.com>
2023-04-06 17:16:38 -0700

    add serialization of username and password. added verify account

--- src/model/WordleSerializer.java
+++ src/model/WordleSerializer.java
@@ -1,43 +1,66 @@
 package model;
 
 import java.io.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
 import java.util.Scanner;
 
 public class WordleSerializer {
     private final String wordsPath = "WordleWords.txt";
-    private final String serializedPath = "database.ser";
-    //private ArrayList<String>[] words;
+    private final String wordsDatabasePath = "database.ser";
+    private final String accountsDatabasePath = "accounts.ser";
     private HashMap<Integer, ArrayList<String>> words;
-    public WordleSerializer(){
-        //words = new ArrayList<ArrayList<String>>();
-        //words = new ArrayList[16];
-        words = new HashMap<Integer, ArrayList<String>>();
+    private ArrayList<WordleAccount> accounts;
 
-        //TODO: load words from file
+    public WordleSerializer(){
+
+        words = new HashMap<>();
+        accounts = new ArrayList<>();
+
         try{
-            FileInputStream rawBytes = new FileInputStream(serializedPath); // Read the .ser file just created
+            FileInputStream rawBytes = new FileInputStream(wordsDatabasePath); // Read the .ser file just created
             ObjectInputStream inFile = new ObjectInputStream(rawBytes);
             //words = (ArrayList<String>[]) inFile.readObject();
             words = (HashMap<Integer, ArrayList<String>>) inFile.readObject();
             inFile.close();
-            System.out.println("Loaded database");
+            System.out.println("Loaded words");
         } catch (IOException | ClassNotFoundException e) {
-            load();
-            save();
-            System.out.println("File not found, creating new database");
+            loadMap();
+            saveMap();
+            System.out.println("words not found, creating new database");
+        }
+        try{
+            FileInputStream rawBytes = new FileInputStream(accountsDatabasePath); // Read the .ser file just created
+            ObjectInputStream inFile = new ObjectInputStream(rawBytes);
+            accounts = (ArrayList<WordleAccount>) inFile.readObject();
+            inFile.close();
+            System.out.println("Loaded accounts");
+        } catch (IOException | ClassNotFoundException e) {
+
+            saveAccounts();
+            System.out.println("accounts not found, creating new database");
         }
 
     }
 
+    public ArrayList<WordleAccount> getAccounts(){
+        return accounts;
+    }
 
-
-    private void save(){
+    private void saveAccounts(){
         try {
-            FileOutputStream bytesToDisk = new FileOutputStream(serializedPath);
+            FileOutputStream bytesToDisk = new FileOutputStream(accountsDatabasePath);
+            ObjectOutputStream outFile = new ObjectOutputStream(bytesToDisk);
+            outFile.writeObject(accounts);
+            outFile.close(); // Always close the output file!
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+    private void saveMap(){
+        try {
+            FileOutputStream bytesToDisk = new FileOutputStream(wordsDatabasePath);
             ObjectOutputStream outFile = new ObjectOutputStream(bytesToDisk);
             outFile.writeObject(words);
             outFile.close(); // Always close the output file!
@@ -47,7 +70,7 @@ public class WordleSerializer {
 
     }
 
-    private void load(){
+    private void loadMap(){
 
         Scanner s=null;
         try {
@@ -65,19 +88,55 @@ public class WordleSerializer {
                 temp.add(word);
                 words.put(word.length(), temp);
             }
-            //words[word.length()-1].add(word);
-            //largest = Math.max(largest, word.length());
-            //words.get()
+
 
         }
 
-        //System.out.println(largest);
 
     }
 
 
     public HashMap<Integer,ArrayList<String>> getMap(){
         return words;
+    }
+
+
+    public WordleAccount verifyLogin(String userName, String password) {
+        if (userName.length() != 0 && password.length() != 0) {
+            for (WordleAccount acct : accounts) {
+                if (acct.getUsername().equals(userName.trim()) && acct.getPassword().equals(password.trim())) {
+                    return acct;
+                }
+            }
+
+            return null;
+        }
+
+        return null;
+    }
+
+    public void update(WordleAccount acct) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getUsername().equals(acct.getUsername())) {
+                accounts.set(i, acct);
+            }
+        }
+
+    }
+    public boolean createNewUser(String newUser, String newPwd) {
+        boolean containsAcct = false;
+        for (WordleAccount acct : accounts) {
+            if (acct.getUsername().equals(newUser)) {
+                containsAcct = true;
+            }
+        }
+        if (!containsAcct) {
+            accounts.add(new WordleAccount(newUser, newPwd));
+
+            return true;
+        }
+
+        return false;
     }
 
 
