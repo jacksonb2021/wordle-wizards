e19ad72f3dbffd7bce0a35a9166cef29c2099397
jacksonb2021 <jacksonburns2021@gmail.com>
2023-04-06 17:03:36 -0700

    wordle account score creation

--- src/model/WordleAccount.java
+++ src/model/WordleAccount.java
@@ -3,10 +3,15 @@ package model;
 public class WordleAccount {
     private final String username;
     private final String password;
+    private int[]score;
 
     public WordleAccount(String username, String password) {
         this.username = username;
         this.password = password;
+        score = new int[6];
+        for(int i=0;i<score.length;i++){
+            score[i]=0;
+        }
     }
 
     public String getUsername(){
@@ -16,5 +21,11 @@ public class WordleAccount {
         return password;
     }
 
+    public void updateScore(int score){
+        this.score[score-1]++;
+    }
 
+    public int[] getScore() {
+        return score;
+    }
 }
