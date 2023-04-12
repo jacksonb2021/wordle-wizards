9d219e9c1d2af7d0d01f7859bcdc216a2fae1be3
unknown <velasquez@sa.arizona.edu>
2023-04-12 13:18:50 -0700

    Added keyboard and login pane to GUI

--- src/view_controller/UsernameLogin.java
+++ src/view_controller/UsernameLogin.java
@@ -1,7 +1,173 @@
 package view_controller;
 
+import java.time.LocalDate;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.WordleAccount;
+import model.WordleSerializer;
+
 
 public class UsernameLogin extends BorderPane {
+	private final WordleSerializer login;
+	private final TextField userTextField = new TextField();
+	private final TextField pwdTextField = new PasswordField();
+	private final Label loginStatus = new Label("Please Login");
+	private final Button loginButton = new Button("Login");
+	private final Button logoutButton = new Button("Log out");
+	private final Button createUserButton = new Button("Create New User");
+	private WordleAccount currentUser;
+	private static final int width = 700;
+	private static final int height = 110;
+	private boolean loggedIn;
 
+	@SuppressWarnings("unused")
+	public UsernameLogin(WordleSerializer login) {
+		this.login = login;
+		loggedIn = false;
+		Stage stage = new Stage();
+		layoutWindow();
+		currentUser = null;
+
+		loginButton.setOnAction(event -> {
+			if (currentUser != null) {
+				loginStatus.setText("You are already logged in, please logout");
+				return;
+			}
+			String userName = userTextField.getText();
+			String password = pwdTextField.getText();
+			currentUser = login.verifyLogin(userName, password);
+			if (currentUser != null) {
+				loginStatus.setText("You are now logged in");
+
+				userTextField.setText("");
+				pwdTextField.setText("");
+				loggedIn = true;
+			} else {
+				loginStatus.setText("Incorrect Username or Password. Try Again");
+				userTextField.setText("");
+				pwdTextField.setText("");
+			}
+
+		});
+
+		logoutButton.setOnAction(event -> {
+			loggedIn = false;
+			currentUser = null;
+			loginStatus.setText("Please Login");
+		});
+
+		createUserButton.setOnAction(event -> {
+			if (!loggedIn) {
+				layoutAddWindow();
+			} else {
+				loginStatus.setText(
+						"You can't create a new user while another user is logged in. Log out first then try again.");
+			}
+
+		});
+
+	}
+
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	public WordleAccount getCurrentUser() {
+		return currentUser;
+	}
+
+	public void setLoginStatus(String update) {
+		loginStatus.setText(update);
+	}
+
+	private void layoutAddWindow() {
+		TextField newUserTextField = new TextField();
+		TextField newPwdTextField = new TextField();
+		BorderPane newWindow = new BorderPane();
+
+		HBox newAccountLane = new HBox();
+		newAccountLane.getChildren().add(new Label("Account Name"));
+		newAccountLane.getChildren().add(newUserTextField);
+		newAccountLane.setAlignment(Pos.BASELINE_CENTER);
+		newAccountLane.setStyle("-fx-padding: 15 40 5 10;");
+		newAccountLane.setSpacing(5);
+
+		HBox newPasswordLane = new HBox();
+		newPasswordLane.getChildren().add(new Label("Password"));
+		newPasswordLane.getChildren().add(newPwdTextField);
+		newPasswordLane.setAlignment(Pos.BASELINE_CENTER);
+		newPasswordLane.setStyle("-fx-padding: 5 0 5 0;");
+		newPasswordLane.setSpacing(5);
+
+		VBox newLoginPane = new VBox();
+		newLoginPane.getChildren().addAll(newAccountLane, newPasswordLane);
+		newWindow.setCenter(newLoginPane);
+
+		HBox buttons = new HBox();
+		Button cancel = new Button("Cancel");
+		Button OK = new Button("OK");
+		buttons.getChildren().addAll(OK, cancel);
+		buttons.setAlignment(Pos.BASELINE_CENTER);
+		buttons.setSpacing(15);
+		newWindow.setBottom(buttons);
+		Stage addWindow = new Stage();
+		Scene addScene = new Scene(newWindow, width / 2, height);
+		addWindow.setTitle("Create New User");
+		addWindow.setScene(addScene);
+		addWindow.show();
+
+		OK.setOnAction(ok -> {
+			if (!login.createNewUser(newUserTextField.getText().trim(), newPwdTextField.getText().trim())) {
+				loginStatus.setText("New User Created");
+				login.saveAccounts();
+			} else {
+				loginStatus.setText("Username is already taken. Try another one.");
+			}
+			addWindow.close();
+		});
+		cancel.setOnAction(Cancel -> {
+			addWindow.close();
+		});
+	}
+
+	private void layoutWindow() {
+		HBox topLabel = new HBox();
+		topLabel.getChildren().add(loginStatus);
+		topLabel.setAlignment(Pos.BASELINE_CENTER);
+
+		HBox accountLane = new HBox();
+		accountLane.getChildren().add(new Label("Account Name"));
+		accountLane.getChildren().add(userTextField);
+		accountLane.getChildren().add(loginButton);
+		accountLane.setAlignment(Pos.BASELINE_CENTER);
+		accountLane.setStyle("-fx-padding: 5 50 5 10;");
+		accountLane.setSpacing(5);
+
+		HBox pwdLane = new HBox();
+		pwdLane.getChildren().add(new Label("Password"));
+		pwdLane.getChildren().add(pwdTextField);
+		pwdLane.getChildren().add(logoutButton);
+		pwdLane.setAlignment(Pos.BASELINE_CENTER);
+		pwdLane.setStyle("-fx-padding: 5 0 5 0;");
+		pwdLane.setSpacing(5);
+
+		HBox newUserLane = new HBox();
+		newUserLane.getChildren().add(createUserButton);
+		newUserLane.setAlignment(Pos.BASELINE_CENTER);
+
+		VBox loginPane = new VBox();
+		loginPane.getChildren().addAll(topLabel, accountLane, pwdLane, newUserLane);
+		this.setCenter(loginPane);
+
+	}
 }
--- src/view_controller/WordleGUI.java
+++ src/view_controller/WordleGUI.java
@@ -1,18 +1,93 @@
 package view_controller;
 
 import javafx.application.Application;
-
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import model.WordleSerializer;
 
 public class WordleGUI extends Application {
+	private UsernameLogin loginPane;
+	private BorderPane everything;
+	private WordleSerializer login;
+
 	@Override
 	public void start(Stage stage) throws Exception {
-		Scene scene = new Scene(new Label("Test"), 900, 600);
+		stage.setTitle("Wordle");
+		login = new WordleSerializer();
+		LayoutGUI();
+		LayoutKeyboard();
+		Scene scene = new Scene(everything, 800, 700);
 
 		stage.setScene(scene);
 		stage.show();
+	}
+
+	private void LayoutKeyboard() {
+		HBox Row1 = new HBox();
+		HBox Row2 = new HBox();
+		HBox Row3 = new HBox();
+		VBox board = new VBox();
+		Button[] keyboardR1 = new Button[10];
+		Button[] keyboardR2 = new Button[9];
+		Button[] keyboardR3 = new Button[7];
+		String letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+		char[] qwerty = letters.toCharArray();
+
+		for (int i = 0; i < 10; i++) {
+			keyboardR1[i] = new Button("" + qwerty[i]);
+			keyboardR1[i].setStyle("-fx-padding: 5 10 10 10;");
+			keyboardR1[i].setFont(new Font("Courier New", 32));
+
+			keyboardR1[i].setOnAction(event -> {
+				Button buttonClicked = (Button) event.getSource();
+				System.out.println(buttonClicked.getText());
+			});
+		}
+		for (int i = 0; i < 9; i++) {
+			keyboardR2[i] = new Button("" + qwerty[i+10]);
+			keyboardR2[i].setStyle("-fx-padding: 5 10 10 10;");
+			keyboardR2[i].setFont(new Font("Courier New", 32));
+
+			keyboardR2[i].setOnAction(event -> {
+				Button buttonClicked = (Button) event.getSource();
+				System.out.println(buttonClicked.getText());
+			});
+		}
+		for (int i = 0; i < 7 ; i++) {
+			keyboardR3[i] = new Button("" + qwerty[i+19]);
+			keyboardR3[i].setStyle("-fx-padding: 5 10 10 10;");
+			keyboardR3[i].setFont(new Font("Courier New", 32));
+			keyboardR3[i].setOnAction(event -> {
+				Button buttonClicked = (Button) event.getSource();
+				System.out.println(buttonClicked.getText());
+			});
+		}
+		Row1.getChildren().addAll(keyboardR1);
+		Row2.getChildren().addAll(keyboardR2);
+		Row3.getChildren().addAll(keyboardR3);
+		Row1.setSpacing(5);
+		Row2.setSpacing(5);
+		Row3.setSpacing(5);
+		Row1.setStyle("-fx-padding: 0 0 0 0;");
+		Row1.setAlignment(Pos.CENTER);
+		Row2.setAlignment(Pos.CENTER);
+		Row3.setAlignment(Pos.CENTER);
+		board.getChildren().addAll(Row1, Row2, Row3);
+		everything.setBottom(board);
+		// TODO Auto-generated method stub
+	}
+
+	private void LayoutGUI() {
+		everything = new BorderPane();
+		loginPane = new UsernameLogin(login);
+		everything.setCenter(loginPane);
 	}
 
 	public static void main(String[] args) {
