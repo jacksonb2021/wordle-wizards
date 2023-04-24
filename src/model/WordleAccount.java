babef4070f23681f8ed9924a3cd635ad96d4c920
jacksonb2021 <jacksonburns2021@gmail.com>
2023-04-23 17:37:15 -0700

    remove unused functions

--- src/model/WordleAccount.java
+++ src/model/WordleAccount.java
@@ -121,35 +121,4 @@ public class WordleAccount implements Serializable{
 		return total;
 	}
 
-//
-//	/**
-//	 *
-//	 * @return
-//	 */
-//	private int totalGuesses() {
-//		int retVal = 0;
-//		for(int i = 0; i < score.length; i++) {
-//			retVal += score[i];
-//		}
-//		return retVal;
-//	}
-//	public int totalScore() {
-//		return totalGuesses() / getTotalGames();
-//	}
-//
-//
-//	/**
-//	 * 	Assumes score is from a game with a 5 letter word.
-//	 * 	Need to record word length and implement general scoring function
-//	 * 	in order to fairly compare scores from different word length games.
-//	 * @param other the object to be compared.
-//	 * @return
-//	 */
-//	@Override
-//	public int compareTo(WordleAccount other) {
-//		//>0 if this is larger than other.
-//		//0 if equal
-//		//<0 if other larger.
-//		return this.totalScore() - other.totalScore();
-//	}
 }
